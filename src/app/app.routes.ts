06f36b7140174d153d81ae24a9723b471018fa07
UriAlcantar <xxjetsonxx@gmail.com>
2025-06-02 12:40:46 -0600

    about me page finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about-me',
+    loadComponent: () => import('./about-me/about-me.component').then(m => m.AboutMeComponent)
+  }
+];
